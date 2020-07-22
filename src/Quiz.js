f4bd746bf450d5a03fd64f85278938b539a84301
Patrick Szczypinski <pizatski@gmail.com>
2020-07-22 14:51:25 -0500

    feature - add quizquestions component and pass props

--- src/Quiz.js
+++ src/Quiz.js
@@ -10,7 +10,11 @@ class Quiz extends Component {
   }
 
  render() {
-   return <div><QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}/></div>
+   return (
+    <div>
+      <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />
+    </div>
+   )
  }
 }
 
--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -1,21 +1,19 @@
 import React, { Component } from "react";
 
 class QuizQuestion extends Component {
-  constructor(props) {
-      super(props)
-  }
-
   render() {
-    return <main>
+    return (
+      <main>
         <section>
-          <p>{this.props.instruction_text}</p>
+          <p>{this.props.quiz_question.instruction_text}</p>
         </section>
         <section className="buttons">
           <ul>
-            <li>{this.props.answer_options}</li>
+            <li>{this.props.quiz_question.answer_options[0]}</li>
           </ul>
         </section>
       </main>
+    )
   }
 }
 
